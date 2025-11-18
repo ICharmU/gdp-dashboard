90ed26dc251a08e848a8eb03ca84c4ed65c16159
Sean Harmon <seanisloveseanislife@gmail.com>
2025-11-17 19:14:10 -0800

    add multiple response outputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,15 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
+    page_title='StackBot WebPage Sandbox',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
 
 def generate_response(query: str):
-    return query[::-1]
+    response = query[::-1]
+    responses = [response] * k
+    return responses
 
 def text_update():
     if st.session_state.user_query:
@@ -20,14 +22,19 @@ def text_update():
 
 if "text" not in st.session_state:
     st.session_state["text"] = ""
-if "response_text" not in st.session_state:
-    st.session_state["response_text"] = ""
+
+k = 10 # number of results
+for i in range(1, k+1):
+    if "response_text" not in st.session_state:
+        st.session_state[f"response_text_{i}"] = ""
 
 user_query = st.text_input("user_query", key="user_query", on_change=text_update, placeholder="Ask a question!")
 
 # Display the response
 if st.session_state["response_text"]:
     st.text(f"Response: {st.session_state['response_text']}")
+
+
 
 
 
