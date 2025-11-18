5883240f248ead23990a34d322924c0238294803
Sean Harmon <seanisloveseanislife@gmail.com>
2025-11-17 19:04:33 -0800

    remove extra text boxes

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,18 +18,10 @@ def text_update():
         response = generate_response(st.session_state.user_query)
         st.session_state["response_text"] = response
 
-def replace_text():
-    st.session_state["text"] = text1.replace(before, after)
-
 if "text" not in st.session_state:
     st.session_state["text"] = ""
 if "response_text" not in st.session_state:
     st.session_state["response_text"] = ""
-
-text1 = st.text_area('Text : ', st.session_state["text"])
-before = st.text_input('Before')
-after = st.text_input('After')
-button = st.button('Button', on_click=replace_text) 
 
 user_query = st.text_input("user_query", key="user_query", on_change=text_update, placeholder="Ask a question!")
 
